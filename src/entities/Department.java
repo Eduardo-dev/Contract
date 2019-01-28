c4336fe1ab4800d8453dfa99ed0257901f566838
Eduardo Nunes <eduardoafonso0519@gmail.com>
2019-01-28 20:47:11 -0200

    Adicionados atributos e métodos de Worker e modificacao no nome do atributo de Department

--- src/entities/Department.java
+++ src/entities/Department.java
@@ -1,18 +1,18 @@
 package entities;
 
 public class Department {
-	private String department;
+	private String name;
 	
 	public Department() {
 	}
-	public Department(String department) {
-		this.department = department;
+	public Department(String name) {
+		this.name = name;
 	}
 	
-	public String getDepartment() {
-		return department;
+	public String getnName() {
+		return name;
 	}
-	public void setDepartment(String department) {
-		this.department = department;
+	public void setName(String name) {
+		this.name = name;
 	}
 }
